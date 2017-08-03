f9c7a9f4561eb2b0105c8d4c017de1fd95c8f832
eshxe <eshxe@mail.ru>
2017-08-03 12:40:33 +0400

    nullable checks

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
@@ -6,7 +8,16 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            return View(new string[] { "C#", "Language", "Features" });
+            List<string> result = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name;
+                decimal? price = p?.Price;
+                result.Add(string.Format("Name: {0}, Price: {1}", name, price));
+            }
+
+            return View(result);
         }
     }
 }
