9501f3c90091c513678b94a14bedf9f69ee8e99e
eshxe <eshxe@mail.ru>
2017-08-11 14:41:17 +0400

    filtering extension adn lambda expressions

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -8,29 +8,46 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            List<string> result = new List<string>();
+            //List<string> result = new List<string>();
 
-            foreach (Product p in Product.GetProducts())
-            {
-                string name = p?.Name ?? "<No Name>";
-                string category = p?.Category;
-                decimal? price = p?.Price ?? 0;
-                string related = p?.Related?.Name ?? "<None>";
-                bool? inStock = p?.InStock;
-                result.Add($"Name: {name}, Category: {category}, Price: {price:C2}, Related: {related}, In stock: {inStock}");
-            }
+            //foreach (Product p in Product.GetProducts())
+            //{
+            //    string name = p?.Name ?? "<No Name>";
+            //    string category = p?.Category;
+            //    decimal? price = p?.Price ?? 0;
+            //    string related = p?.Related?.Name ?? "<None>";
+            //    bool? inStock = p?.InStock;
+            //    result.Add($"Name: {name}, Category: {category}, Price: {price:C2}, Related: {related}, In stock: {inStock}");
+            //}
 
-            Dictionary<string, Product> products = new Dictionary<string, Product>
+            //Dictionary<string, Product> products = new Dictionary<string, Product>
+            //{
+            //    ["Kayak"] = new Product {Name = "Kayak", Price = 275M} ,
+            //    ["Life jacket"] = new Product {Name = "Life jacket", Price = 48.95M}
+            //};
+
+            //decimal cartTotal = cart.TotalPrices();
+
+            //return View("Index", new string[] { $"Total: {cartTotal:C2}" });
+
+            Product[] productArray =
             {
-                ["Kayak"] = new Product {Name = "Kayak", Price = 275M} ,
-                ["Life jacket"] = new Product {Name = "Life jacket", Price = 48.95M}
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Life jacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
             };
 
             ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
 
-            decimal cartTotal = cart.TotalPrices();
+            decimal priceFilterTotal = productArray.FilterByPrice(20).TotalPrices();
+            decimal nameFilterTotal = productArray.FilterByName('S').TotalPrices();
 
-            return View("Index", new string[] { $"Total: {cartTotal:C2}" });
+            return View("Index", new string[]
+            {
+                $"Price Total: {priceFilterTotal:C2}",
+                $"Name Total: {nameFilterTotal:C2}"
+            });
         }
     }
 }
--- LanguageFeatures/Models/MyExtensionMethods.cs
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -1,16 +1,43 @@
-﻿namespace LanguageFeatures.Models
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LanguageFeatures.Models
 {
     public static class MyExtensionMethods
     {
-        public static decimal TotalPrices(this ShoppingCart cartParams)
+        public static decimal TotalPrices(this IEnumerable<Product> products)
         {
             decimal total = 0;
 
-            foreach(Product prod in cartParams.Products)
+            foreach(Product prod in products)
             {
                 total += prod?.Price ?? 0;
             }
             return total;
+        }
+
+        public static IEnumerable<Product> FilterByPrice(
+            this IEnumerable<Product> productEnum, decimal minimumPrice)
+        {
+            foreach(Product prod in productEnum)
+            {
+                if((prod?.Price ?? 0) >= minimumPrice)
+                {
+                    yield return prod;
+                }
+            }
+        }
+
+        public static IEnumerable<Product> FilterByName(
+            this IEnumerable<Product> productEnum, char firstLetter)
+        {
+            foreach(Product prod in productEnum)
+            {
+                if (prod?.Name?[0] == firstLetter)
+                {
+                    yield return prod;
+                }
+            }
         }
     }
 }
